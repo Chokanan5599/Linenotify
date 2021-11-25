860563041c1138963d70f4b0dd45059e1162492f
Chokanan5599 <chokanan5599@gmail.com>
2021-11-25 15:06:54 +0700

    make it better

--- index.js
+++ index.js
@@ -6,17 +6,18 @@
  * @desc A sample project of Node.js and Line API
  */
 const server = require("express");
+const express = server();
 const PORT = process.env.PORT || 9999;
 const request = require("request");
 const bodyParser = require("body-parser");
-server()
+express()
   .use(bodyParser.json())
   .use(bodyParser.urlencoded({ extended: false }))
   .get("/", (req, res) =>
     res.send(`Hi there! This is a nodejs-line-api running on PORT: ${PORT}`)
   )
   // เพิ่มส่วนของ Webhook เข้าไป
-  .get("/webhook", function (req, res) {
+  .post("/webhook", function (req, res) {
     let replyToken = req.body.events[0].replyToken;
     let msg = req.body.events[0].message.text;
 
