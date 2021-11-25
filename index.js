c7b7df3de195b06e41951eceddeccc6f41e13172
Chokanan5599 <chokanan5599@gmail.com>
2021-11-25 09:41:25 +0700

    make it better

--- index.js
+++ index.js
@@ -18,11 +18,8 @@ const bodyParser = require("body-parser");
 server()
   .use(bodyParser.json())
   .use(bodyParser.urlencoded({ extended: false }))
-  .get(
-    "/",
-    (req, res) =>
-      res.send(`Hi there! This is a nodejs-line-api running on PORT: ${PORT}`),
-    console.log("Seccessfull_Notify_line")
+  .get("/", (req, res) =>
+    res.send(`Hi there! This is a nodejs-line-api running on PORT: ${PORT}`)
   )
 
   // เพิ่มส่วนของ Webhook เข้าไป
