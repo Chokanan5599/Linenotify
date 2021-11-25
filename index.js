82ade15afe7db0381393c66e209fb90e755eb3dd
Chokanan5599 <chokanan5599@gmail.com>
2021-11-25 15:46:05 +0700

    make it better

--- index.js
+++ index.js
@@ -1,35 +1,21 @@
 /**
  * @author Pamontep Panya
  * @email pamontep.p@gmail.com
- * @create date 2018-06-01 01:07:04
- * @modify date 2018-06-01 01:07:04
- * @desc This file used to be a Main file of NodeJS API
+ * @create date 2018-06-03 03:45:02
+ * @modify date 2018-06-03 03:45:02
+ * @desc A sample project of Node.js and Line API
  */
-const moment = require("moment");
 const server = require("express");
-const path = require("path");
 const PORT = process.env.PORT || 9999;
 const request = require("request");
 const bodyParser = require("body-parser");
-const status = require("../nodejs-express-api-init/src/classes/Status");
 
 server()
   .use(bodyParser.json())
   .use(bodyParser.urlencoded({ extended: false }))
   .get("/", (req, res) =>
-    res.send(`Hi there! This is a nodejs-line-api running on PORT: ${PORT}`)
+    res.send(
+      `Hi there! This is a nodejs-line-api running on PORT  TEST: ${PORT}`
+    )
   )
-  // เพิ่มส่วนของ Webhook เข้าไป
-  .post("/webhook", function (req, res) {
-    let replyToken = req.body.events[0].replyToken;
-    let msg = req.body.events[0].message.text;
-
-    console.log(`Message token : ${replyToken}`);
-    console.log(`Message from chat : ${msg}`);
-
-    res.json({
-      status: 200,
-      message: `Webhook is working!`,
-    });
-  })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
