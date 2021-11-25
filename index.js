53f4cf729e76c883ededd46cf0ad97ffbe451451
Chokanan5599 <chokanan5599@gmail.com>
2021-11-25 17:08:28 +0700

    make it better

--- index.js
+++ index.js
@@ -20,11 +20,11 @@ server()
   )
   // เพิ่มส่วนของ Webhook เข้าไป
   .post("/webhook", (req, res) => {
-    let replyToken = req.body.events[0].replyToken;
-    let msg = req.body.events[0].message.text;
+    // let replyToken = req.body.events[0].replyToken;
+    // let msg = req.body.events[0].message.text;
 
-    console.log(`Message token : ${replyToken}`);
-    console.log(`Message from chat : ${msg}`);
+    // console.log(`Message token : ${replyToken}`);
+    // console.log(`Message from chat : ${msg}`);
 
     res.json({
       status: 200,
@@ -32,22 +32,3 @@ server()
     });
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-// server()
-//   .use(bodyParser.json())
-//   .use(bodyParser.urlencoded({ extended: false}))
-//   .get('/', (req, res) => res.send(`Hi there! This is a nodejs-line-api running on PORT: ${ PORT }`))
-//   // เพิ่มส่วนของ Webhook เข้าไป
-//   .post('/webhook', function (req, res) {
-//       let replyToken = req.body.events[0].replyToken;
-//       let msg = req.body.events[0].message.text;
-
-//       console.log(`Message token : ${ replyToken }`);
-//       console.log(`Message from chat : ${ msg }`);
-
-//       res.json({
-//           status: 200,
-//           message: `Webhook is working!`
-//       });
-//   })
-//   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
