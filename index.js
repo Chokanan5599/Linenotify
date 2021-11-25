827d48d409cc325c7ed204865de5265479b7b169
Chokanan5599 <chokanan5599@gmail.com>
2021-11-25 11:00:15 +0700

    make it better

--- index.js
+++ index.js
@@ -25,21 +25,10 @@ server()
   .use(bodyParser.urlencoded({ extended: false }))
   .get("/", (req, res) =>
     res.send(`Hi there! This is a nodejs-line-api running on PORT: ${PORT}`)
-  )
-  // เพิ่มส่วนของ Webhook เข้าไป
-  //   .post("/webhook", function (req, res) {
-  //     let replyToken = req.body.events[0].replyToken;
-  //     let msg = req.body.events[0].message.text;
+  );
 
-  //     console.log(`Message token : ${replyToken}`);
-  //     console.log(`Message from chat : ${msg}`);
-
-  //     res.json({
-  //       status: 200,
-  //       message: `Webhook is working!`,
-  //     });
-  //   })
-  .app.post("/webhook", function (req, res) {
+app
+  .post("/webhook", function (req, res) {
     try {
       const validator = new hmacValidator();
       const notificationRequestItems = req.body.notificationRequestItems;
