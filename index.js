c8b7eaf7486a2e7b0a86e05d90cff2ae679c621c
Chokanan5599 <chokanan5599@gmail.com>
2021-11-25 10:56:01 +0700

    make it better

--- index.js
+++ index.js
@@ -5,10 +5,15 @@
  * @modify date 2018-06-03 03:45:02
  * @desc A sample project of Node.js and Line API
  */
+const expreess = require("express");
+const path = require("path");
+const hbs = require("express-handlebars");
 const server = require("express");
 const PORT = process.env.PORT || 9999;
 const request = require("request");
 const bodyParser = require("body-parser");
+const { Client, Config, hmacValidator } = require("@adyen/api-library");
+const { post } = require("request");
 // const functions = require("firebase-functions");
 
 // exports.Webhook = functions.https.onRequest((res, res) => {
@@ -22,16 +27,46 @@ server()
     res.send(`Hi there! This is a nodejs-line-api running on PORT: ${PORT}`)
   )
   // เพิ่มส่วนของ Webhook เข้าไป
-  .post("/webhook", function (req, res) {
-    let replyToken = req.body.events[0].replyToken;
-    let msg = req.body.events[0].message.text;
+  //   .post("/webhook", function (req, res) {
+  //     let replyToken = req.body.events[0].replyToken;
+  //     let msg = req.body.events[0].message.text;
 
-    console.log(`Message token : ${replyToken}`);
-    console.log(`Message from chat : ${msg}`);
+  //     console.log(`Message token : ${replyToken}`);
+  //     console.log(`Message from chat : ${msg}`);
 
-    res.json({
-      status: 200,
-      message: `Webhook is working!`,
-    });
+  //     res.json({
+  //       status: 200,
+  //       message: `Webhook is working!`,
+  //     });
+  //   })
+  .app.post("/webhook", function (req, res) {
+    try {
+      const validator = new hmacValidator();
+      const notificationRequestItems = req.body.notificationRequestItems;
+      console.log("notification Request");
+      console.log(notificationRequestItems);
+
+      notificationRequestItems.forEach((item) => {
+        if (
+          validator.hmacValidator(
+            Item.notificationRequestItems,
+            process.env.HMAC_KEY
+          )
+        ) {
+          const eventCode = item.notificationRequestItems.eventCode;
+          if (eventCode === "AUTHORISED") {
+            if (item.notificationRequestItems.success === "true") {
+            } else {
+            }
+          } else {
+            console.log("Non-Valid NotificationRequestItems");
+          }
+        }
+      });
+      req.send("[accepted!]");
+    } catch (error) {
+      console.error(error);
+    }
   })
+
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
