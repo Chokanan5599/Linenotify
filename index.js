a8c244072d841768d4df2332e91884662da51e1a
Chokanan5599 <chokanan5599@gmail.com>
2021-11-25 15:13:39 +0700

    make it better

--- index.js
+++ index.js
@@ -1,26 +1,47 @@
 /**
  * @author Pamontep Panya
  * @email pamontep.p@gmail.com
- * @create date 2018-06-03 03:45:02
- * @modify date 2018-06-03 03:45:02
- * @desc A sample project of Node.js and Line API
+ * @create date 2018-06-01 01:07:04
+ * @modify date 2018-06-01 01:07:04
+ * @desc This file used to be a Main file of NodeJS API
  */
+const moment = require("moment");
 const server = require("express");
-const express = server();
+const path = require("path");
 const PORT = process.env.PORT || 9999;
 const request = require("request");
 const bodyParser = require("body-parser");
-express()
+const status = require("../nodejs-express-api-init/src/classes/Status");
+
+server()
   .use(bodyParser.json())
   .use(bodyParser.urlencoded({ extended: false }))
   .get("/", (req, res) =>
-    res.send(`Hi there! This is a nodejs-line-api running on PORT: ${PORT}`)
+    res.send(
+      `Hi there! This is a nodejs-express-api-init running on PORT: ${PORT}`
+    )
   )
-  // เพิ่มส่วนของ Webhook เข้าไป
-  .post("/webhook", function (req, res) {
-    res.send({
+  .get("/checkStatus", function (req, res) {
+    status.getStatusFromFile().then(function (rs) {
+      if (typeof rs.result === "boolean" && rs.result === true) {
+        res.json({
+          result: true,
+          status: 200,
+          message: `Successful! Latest status is ${rs.data}`,
+        });
+      } else {
+        res.json({
+          result: false,
+          status: 500,
+          message: "Error! Internal Server Error",
+        });
+      }
+    });
+  })
+  .use("/postStatus", function (req, res) {
+    res.json({
+      result: true,
       status: 200,
-      message: `Webhook is working!`,
     });
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
