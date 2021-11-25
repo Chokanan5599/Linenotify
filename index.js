6daf8401129cce356162a6f22461f22d4bf63ed1
Chokanan5599 <chokanan5599@gmail.com>
2021-11-25 09:52:58 +0700

    make it better

--- index.js
+++ index.js
@@ -18,15 +18,13 @@ const bodyParser = require("body-parser");
 server()
   .use(bodyParser.json())
   .use(bodyParser.urlencoded({ extended: false }))
-  //   .get("/", (req, res) =>
-  //     res.send(`Hi there! This is a nodejs-line-api running on PORT: ${PORT}`)
-  //   )
-
+  .get("/", (req, res) =>
+    res.send(`Hi there! This is a nodejs-line-api running on PORT: ${PORT}`)
+  )
   // เพิ่มส่วนของ Webhook เข้าไป
   .post("/webhook", function (req, res) {
     let replyToken = req.body.events[0].replyToken;
     let msg = req.body.events[0].message.text;
-    console.log(replyToken);
 
     console.log(`Message token : ${replyToken}`);
     console.log(`Message from chat : ${msg}`);
