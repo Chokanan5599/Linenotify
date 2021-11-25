d998ae07de50ae6bca50e77237192e045d40727c
Chokanan5599 <chokanan5599@gmail.com>
2021-11-25 14:00:42 +0700

    make it better

--- index.js
+++ index.js
@@ -30,3 +30,20 @@ server()
     });
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+// @app.route("/callback", methods=['POST'])
+// def callback():
+//     # get X-Line-Signature header value
+//     signature = request.headers['X-Line-Signature']
+
+//     # get request body as text
+//     body = request.get_data(as_text=True)
+//     app.logger.info("Request body: " + body)
+
+//     # handle webhook body
+//     try:
+//         handler.handle(body, signature)
+//     except InvalidSignatureError:
+//         abort(400)
+
+//     return 'OK'
