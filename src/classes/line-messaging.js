94a93f0e2a2a6be537fb400b74f7e6bd4ee77925
Chokanan5599 <chokanan5599@gmail.com>
2021-11-26 15:59:52 +0700

    make it better

--- src/classes/line-messaging.js
+++ src/classes/line-messaging.js
@@ -6,7 +6,6 @@
  * @desc A class that using for handle message that got from Line Chat.
  */
 const lineApiService = require("../services/line-api-service");
-const firebaseService = require("../services/firebase-service");
 
 class LineMessaging {
   constructor() {}
@@ -20,23 +19,9 @@ class LineMessaging {
             text: message,
           },
         ];
-
-        if (message == "hogwarts") {
-          return firebaseService.getHogwartHouses().then(function (rsHouses) {
-            _messages[0].text = rsHouses;
-            return lineApiService
-              .reply(replyToken, _messages)
-              .then(function (rs) {
-                return resolve(rs);
-              });
-          });
-        } else {
-          return lineApiService
-            .reply(replyToken, _messages)
-            .then(function (rs) {
-              return resolve(rs);
-            });
-        }
+        return lineApiService.reply(replyToken, _messages).then(function (rs) {
+          return resolve(rs);
+        });
       } catch (e) {
         return reject(e);
       }
