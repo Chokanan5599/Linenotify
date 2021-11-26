e34147c6c15ef717c77d2e8a4a917e6a5065fc61
Chokanan5599 <chokanan5599@gmail.com>
2021-11-26 15:53:38 +0700

    make it better

--- index.js
+++ index.js
@@ -1,38 +1,3 @@
-// /**
-//  * @author Pamontep Panya
-//  * @email pamontep.p@gmail.com
-//  * @create date 2018-06-03 03:45:02
-//  * @modify date 2018-06-03 03:45:02
-//  * @desc A sample project of Node.js and Line API
-//  */
-// const server = require("express");
-// const PORT = process.env.PORT || 9999;
-// const request = require("request");
-// const bodyParser = require("body-parser");
-
-// server()
-//   .use(bodyParser.json())
-//   .use(bodyParser.urlencoded({ extended: false }))
-//   .get("/", (req, res) =>
-//     res.send(
-//       `Hi there! This is a nodejs-line-api running on PORT  TEST: ${PORT}`
-//     )
-//   )
-//   // เพิ่มส่วนของ Webhook เข้าไป
-//   .post("/webhook", (req, res) => {
-//     // let replyToken = req.body.events[0].replyToken;
-//     // let msg = req.body.events[0].message.text;
-
-//     // console.log(`Message token : ${replyToken}`);
-//     // console.log(`Message from chat : ${msg}`);
-
-//     res.json({
-//       status: 200,
-//       message: `Webhook is working!`,
-//     });
-//   })
-//   .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
 /**
  * @author Pamontep Panya
  * @email pamontep.p@gmail.com
@@ -44,29 +9,64 @@ const server = require("express");
 const PORT = process.env.PORT || 9999;
 const request = require("request");
 const bodyParser = require("body-parser");
-const lineMessaging = require("./src/classes/line-messaging");
 
 server()
   .use(bodyParser.json())
   .use(bodyParser.urlencoded({ extended: false }))
   .get("/", (req, res) =>
-    res.send(`Hi there! This is a nodejs-line-api running on PORT: ${PORT}`)
+    res.send(
+      `Hi there! This is a nodejs-line-api running on PORT  TEST: ${PORT}`
+    )
   )
   // เพิ่มส่วนของ Webhook เข้าไป
-  .post("/webhook", function (req, res) {
-    let replyToken = req.body.events[0].replyToken;
-    let message = req.body.events[0].message.text;
+  .post("/webhook", (req, res) => {
+    // let replyToken = req.body.events[0].replyToken;
+    // let msg = req.body.events[0].message.text;
 
-    console.log(`Message token : ${replyToken}`);
-    console.log(`Message from chat : ${message}`);
+    // console.log(`Message token : ${replyToken}`);
+    // console.log(`Message from chat : ${msg}`);
 
-    lineMessaging.replyMessage(replyToken, message).then(function (rs) {
-      console.log(`Reply message result : ${rs}`);
-
-      res.json({
-        status: 200,
-        message: `Sent message!`,
-      });
+    res.json({
+      status: 200,
+      message: `Webhook is working!`,
     });
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+// /**
+//  * @author Pamontep Panya
+//  * @email pamontep.p@gmail.com
+//  * @create date 2018-06-03 03:45:02
+//  * @modify date 2018-06-03 03:45:02
+//  * @desc A sample project of Node.js and Line API
+//  */
+// const server = require("express");
+// const PORT = process.env.PORT || 9999;
+// const request = require("request");
+// const bodyParser = require("body-parser");
+// const lineMessaging = require("./src/classes/line-messaging");
+
+// server()
+//   .use(bodyParser.json())
+//   .use(bodyParser.urlencoded({ extended: false }))
+//   .get("/", (req, res) =>
+//     res.send(`Hi there! This is a nodejs-line-api running on PORT: ${PORT}`)
+//   )
+//   // เพิ่มส่วนของ Webhook เข้าไป
+//   .post("/webhook", function (req, res) {
+//     let replyToken = req.body.events[0].replyToken;
+//     let message = req.body.events[0].message.text;
+
+//     console.log(`Message token : ${replyToken}`);
+//     console.log(`Message from chat : ${message}`);
+
+//     lineMessaging.replyMessage(replyToken, message).then(function (rs) {
+//       console.log(`Reply message result : ${rs}`);
+
+//       res.json({
+//         status: 200,
+//         message: `Sent message!`,
+//       });
+//     });
+//   })
+//   .listen(PORT, () => console.log(`Listening on ${PORT}`));
